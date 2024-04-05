8d0da9e55bd23f0f910c4daa93187e382afa2454
Chia Rena <Chocorena@gmail.com>
2024-04-05 13:56:39 +0800

    added more test cases to DeleteLessonCommand

--- src/test/java/seedu/address/logic/commands/DeleteLessonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteLessonCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
 
@@ -11,8 +12,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-
+import seedu.address.model.student.Student;
 
 
 class DeleteLessonCommandTest {
@@ -24,6 +24,19 @@ class DeleteLessonCommandTest {
         DeleteLessonCommand deleteLessonCommand = new DeleteLessonCommand(outOfBoundIndex, Index.fromZeroBased(0));
 
         assertThrows(CommandException.class, () -> deleteLessonCommand.execute(model));
+    }
+
+    @Test
+    public void execute_invalidLessonIndex_throwsCommandException() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Student studentToEdit = model.getFilteredStudentList().get(0); // Assuming this student exists
+        Index studentIndex = Index.fromZeroBased(0);
+        Index outOfBoundLessonIndex = Index.fromOneBased(studentToEdit
+                .getLessons().size() + 1); // Non-existent lesson index
+
+        DeleteLessonCommand deleteLessonCommand = new DeleteLessonCommand(outOfBoundLessonIndex, studentIndex);
+
+        assertCommandFailure(deleteLessonCommand, model, "The lesson index provided is invalid.");
     }
 
 
